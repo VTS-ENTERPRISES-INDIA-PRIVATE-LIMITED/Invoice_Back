6adedcd7910e004c2a8e8416485e4b711532e1d9
subrahmanyam14 <d.balasubrahmanyam4@gmail.com>
2024-08-06 17:19:39 +0530

    added welcome message

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api', router);
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome to invoice backend....");
+})
 
 const PORT = process.env.PORT || 5000;
 
